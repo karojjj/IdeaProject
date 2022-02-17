e2145242b58c4e5ab86a87fe642437f551a15fd6
karojjj <karojjj@gmail.com>
2022-02-17 16:02:26 -0500

    all java codes

---
+++ com/syntax/class01/PrimitiveDataTypes.java
@@ -0,0 +1,69 @@
+package com.syntax.class01;
+
+public class PrimitiveDataTypes {
+
+	public static void main(String[] args) {
+		
+		//1 create variable =box=variable
+		
+		/* to create a variable we need:
+		 *           1. specify datatype
+		 *           2.give a name
+		 */
+
+		//datatypes to represent numeric value
+		
+		byte box1=127;
+		
+		short box2=32767;
+		
+		int box3=327677666; //most used
+		int age = 30;
+		System.out.println(age);
+		long box4=4057575758848387575L; // will be using
+		
+		//Data types to represent decimals
+		
+		  float price=12.99F;
+		  
+		  double price1=13.99; //most used
+		  
+		  //to represent a single character
+		
+          char gender= 'F';
+          
+          char sign='$';
+          
+          //to represent true or false
+          
+          boolean snow=false;
+          boolean tired=true;
+          
+          //I want to print 13.99???
+          
+          System.out.println(price1);
+          
+          System.out.println("price1");
+          
+          System.out.println(sign);
+          
+          System.out.println("sign");
+          
+          
+          //String value: "speaker"
+          //String variable: 
+          //String is built out of characters.
+          String myFavoriteWord="speaker";
+          System.out.println(myFavoriteWord);
+          
+          String studentName="Medine";
+          System.out.println(studentName);
+          String studentLastName="Kaya";
+          System.out.println(studentLastName);
+          
+  
+          
+          
+	}
+
+}
---
+++ com/syntax/class05/LogicalOr.java
@@ -0,0 +1,23 @@
+package com.syntax.class05;
+
+import java.util.Scanner;
+
+public class LogicalOr {
+
+	public static void main(String[] args) {
+		// or II
+		String day = "Saturday";
+		if (day.equals("Saturday") || day.equals("Sunday")) {
+			System.out.println("I have a JAVA Class");
+		} else {
+			System.out.println("Code outside of if statement");
+
+		}
+		 
+		
+		
+		
+		
+	}
+
+}
